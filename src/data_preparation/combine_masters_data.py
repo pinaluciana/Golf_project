d86655ac086a670b5702b4a728b67cfdcba594be
Luciana Piña Strzelecki <luciana.pinastrzelecki@unil.ch>
2025-12-03 14:16:35 +0000

    Created new folder: src/data_preparation/ and included inside __init__.py file, moved from old location the files to combine the data of different years per major, created one new file: combine_all_data.py (before this code was inside data_loader.py, so decided to clean it up

---
+++ src/data_preparation/combine_masters_data.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import pandas as pd
+
+def main():
+    folder = Path(__file__).parent
+    
+    files = sorted(folder.glob("*.csv"))
+    
+    dfs = []
+    
+    for f in files:
+        if "combined" in f.name:
+            continue
+        
+        year = f.name.split("_")[0]
+        
+        df = pd.read_csv(f)
+
+        #remove players who didn't make the cut, got disqualified("DQ") or withdrew("WD")
+        df = df[~df["position"].isin(["CUT", "DQ", "WD"])]
+        
+        df["year"] = int(year)
+        dfs.append(df)
+        
+    all_masters = pd.concat(dfs, ignore_index=True)
+    
+    project_root = folder.parents[2]
+    
+    processed_folder = project_root / "data" / "processed" / "The_Masters"
+    processed_folder.mkdir(parents=True, exist_ok=True)
+    
+    output_path = processed_folder / "Masters_combined_data.csv"
+    all_masters.to_csv(output_path, index=False)
+    
+    print(f"Saved combined file to {output_path}")
+    print(f"Total rows: {len(all_masters)}")
+    print(f"Years included: {sorted(all_masters['year'].unique())}")
+    print(all_masters.head())
+
+if __name__ == "__main__":
+    main()
